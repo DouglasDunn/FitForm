ab210aed3cf1e3a45a99e5dcc7e2509bb4cc1595
Douglas Dunn <douglas.dunn@outlook.com>
2018-10-15 19:19:02 -0700

    potential fix editted store.js on the client side

--- server.js
+++ server.js
@@ -39,14 +39,14 @@ app.use('/api/goals', goals);
 app.use('/api/daily-progress', dailyProgress);
 
 // Server static assets if in production
-// if (1) {
-//   // Set static folder
-//   app.use(express.static('client/build'));
-//
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
