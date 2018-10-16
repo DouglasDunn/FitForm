e04a8a62142f81d9c3dc43d8cf5242fd6c4be6fb
Douglas Dunn <douglas.dunn@outlook.com>
2018-10-15 19:37:01 -0700

    editted store.js to fix

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,12 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
